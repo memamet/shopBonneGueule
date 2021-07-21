bd14d263265b95b2ff21890a97edee58c900daf9
mamet <max-eugene.mamet@student.isae-supaero.fr>
2021-07-21 17:16:41 +0200

    delete index in views

--- backend/shop_project/views.py
+++ backend/shop_project/views.py
@@ -5,12 +5,6 @@ from rest_framework import viewsets
 from .serializers import ApparelSerializer
 
 
-
-def index(request):
-    message = "Salut tout le monde !"
-    return HttpResponse(message)
-
-
 class ApparelView(viewsets.ModelViewSet):
     serializer_class = ApparelSerializer
     queryset = Apparel.objects.all()
