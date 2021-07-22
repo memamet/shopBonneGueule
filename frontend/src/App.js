ee4233e2337367ec9bb1f7eedbb9e61f0f447375
mamet <max-eugene.mamet@student.isae-supaero.fr>
2021-07-22 22:22:53 +0200

    questions frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,70 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, CSSProperties } from "react";
 import axios from "axios";
-
-const App = () => {
-  const [apparelList, setApparelList] = useState([]);
-  const [priceRangeList, setPriceRangeList] = useState([]);
+import { Form, FormGroup, Label, Input } from "reactstrap";
 
 
-
-  useEffect(() => {
-    axios.get("/api/apparels/").then((res) => setApparelList(res.data));
-    axios.get("/api/price-ranges/").then((res) => setPriceRangeList(res.data));
-  }, []);
-
-  const apparelOptions = apparelList.map((apparel) => {
-    return <option value={apparel.name}>{apparel.name}</option>;
-  });
-  const priceRangeOptions = priceRangeList.map((priceRange) => {
-    return <option value = {priceRange.label}>{priceRange.label}</option>
-  });
-  return (
-    <div className="App">
-      <select>{apparelOptions}</select>
-      <select>{priceRangeOptions}</select>
-    </div>
-  );
+// For using reactstrap components, use
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 };
 
+
+const App = () => {
+
+  
+
+  // Initialise une constante apparelList avec la valeur []
+  // Nous donne accès à une méthode setApparelList pour modi
+  const [apparelList, setApparelList] = useState([]);
+  useEffect(() => {
+    axios.get("/api/apparels/").then((res) => {
+      setApparelList(res.data);});}, []);
+  const apparelOptions = apparelList.map((apparel) => {
+    return <option 
+      value={apparel.name}
+    >{apparel.name}</option>;
+  });
+
+  const [priceRangeList, setPriceRangeList] = useState([]);
+  useEffect(() => {
+    axios.get("/api/price-ranges/").then((res) => {
+      setPriceRangeList(res.data);});}, []);
+  const priceRangeOptions = priceRangeList.map((priceRange) => {
+    return <option 
+      value={priceRange.label}
+    >{priceRange.label}</option>;
+  });
+
+
+  return (
+    <div>
+      <div className="App" style={styles.container}>
+        <Form>
+          <FormGroup>
+            <Label for="apparelSelect">Choisir un vêtement</Label>
+            <Input type="select" name="apparelSelect" id="apparelSelect">
+              {apparelOptions}
+            </Input>
+          </FormGroup>
+        </Form>
+      </div>
+      <div className="App" style={styles.container}>
+      <Form>
+        <FormGroup>
+          <Label for="priceRangeSelect">Choisir un prix</Label>
+          <Input type="select" name="priceRangeSelect" id="priceRangeSelect">
+            {priceRangeOptions}
+          </Input>
+        </FormGroup>
+      </Form>
+    </div>
+  </div>
+  );
+};
 export default App;
