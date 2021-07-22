4f4bf6523b8a1c5efe4c37f439e0988ff202de8a
mamet <max-eugene.mamet@student.isae-supaero.fr>
2021-07-22 11:05:37 +0200

    priceRange frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,25 @@ import axios from "axios";
 
 const App = () => {
   const [apparelList, setApparelList] = useState([]);
+  const [priceRangeList, setPriceRangeList] = useState([]);
+
+
 
   useEffect(() => {
-    axios.get("/api/apparels/").then((res) => {
-      setApparelList(res.data);
-    });
+    axios.get("/api/apparels/").then((res) => setApparelList(res.data));
+    axios.get("/api/price-ranges/").then((res) => setPriceRangeList(res.data));
   }, []);
 
   const apparelOptions = apparelList.map((apparel) => {
     return <option value={apparel.name}>{apparel.name}</option>;
   });
-
+  const priceRangeOptions = priceRangeList.map((priceRange) => {
+    return <option value = {priceRange.label}>{priceRange.label}</option>
+  });
   return (
     <div className="App">
       <select>{apparelOptions}</select>
+      <select>{priceRangeOptions}</select>
     </div>
   );
 };
