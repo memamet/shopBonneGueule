0c97a0a704ba2aff926770a8cb6f2a8833e1bbd0
mamet <max-eugene.mamet@student.isae-supaero.fr>
2021-07-22 10:09:42 +0200

    cors fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [apparelList, setApparelList] = useState([]);
 
   useEffect(() => {
-    axios.get("api/apparels").then((res) => {
+    axios.get("/api/apparels/").then((res) => {
       setApparelList(res.data);
     });
   }, []);
